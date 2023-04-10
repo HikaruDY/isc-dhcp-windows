5a47e5b09fecfc501a16dd0d3bdb5d44c421ff61
Hikaru Kurosaki <Hikaru@oss.xprj.org>
2023-04-10 17:30:45 +0900

    New Build.sh / Build.cmd

--- Windows/Build.cmd
+++ Windows/Build.cmd
@@ -1,38 +1,25 @@
 @echo off
-pushd "%~dp0"
 REM /*
 REM // ISC DHCPd 4.x for Windows (with Cygwin) patch
-REM //	(C)2016 Hikaru Kurosaki
+REM //	(C)2016-2023 Hikaru Kurosaki
 REM //	http://xprj.org/
 REM */
 
-gcc -c VirtualFunction.c
-IF NOT EXIST "VirtualFunction.o" call :ERR "Compile failed: VirtualFuncition.c"
-
-pushd ..
-
-	IF NOT EXIST "Makefile" sh configure
-	IF NOT EXIST "Makefile" call :ERR "./configure failed."
-
-	REM //Script directory
-	set BASE=%~dp0
-	set BASE=%BASE::=%
-	set BASE=%BASE:\=/%
-	set BASE=/cygdrive/%BASE%
-	set DSTDIR=%BASE%/DHCP
-
-	rd /s /q "%~dp0\DHCP"
-	mkdir "%~dp0\DHCP"
-
- 	make LDFLAGS="%~dp0\VirtualFunction.o" CFLAGS="-g -O2 -fno-strict-aliasing -I%BASE%/../includes -I%BASE%/../bind/include -I%BASE%" %* || GOTO ERR "Make failed"
-	make install prefix="%DSTDIR%" || GOTO ERR "Install failed"
-
-	echo "I: Build successful. Output directory is '%DSTDIR%'"
-
+:MAIN
+pushd "%~dp0"
+	CALL :BUILD %*
 popd
-popd
+GOTO :EOF
 
-GOTO EOF
+
+
+:BUILD
+	setlocal
+	set DHCP_BUILD=1
+	bash ./Build.sh %*
+	endlocal
+GOTO :EOF
+
 :ERR
 echo E: %~1
 exit /b 1
